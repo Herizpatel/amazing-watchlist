cf0e41a15019340112f83905352ee9fd94d10feb
Heriz <herrypatel3369@gmail.com>
2021-03-07 12:07:02 -0500

    7 February

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Show} from '../models/show.model';
 
 @Component({
   selector: 'app-search-result',
@@ -6,8 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-result.component.css']
 })
 export class SearchResultComponent implements OnInit {
-
-  constructor() { }
+  shows: Array<Show>;
+  constructor() {
+    this.shows = [ new Show({
+      name: 'Doctor Who',
+      language: 'English',
+      genres: ['Sci-Fi', ' Action', ' Adventure'],
+      id: 210,
+      summary: 'A show about something',
+      status: 'ongoing',
+      image: 'https://culvercitycrossroads.com/wp-content/uploads/2016/02/talent-show.jpg'
+    }),
+      new Show({
+        name: 'The Magicians',
+        language: 'English',
+        genres: ['Sci-Fi', 'Action', 'Adventure'],
+        id: 210,
+        summary: 'A show about magicians and their trauma.',
+        status: 'ongoing',
+        image: 'https://static.tvmaze.com/uploads/images/medium_portrait/231/579166.jpg'
+      }),
+      new Show({
+        name: 'Smallville',
+        language: 'English',
+        genres: ['Sci-Fi', 'Action', 'Adventure'],
+        id: 210,
+        summary: 'A show about Superman as a teenager.',
+        status: 'ongoing',
+        image: 'https://static.tvmaze.com/uploads/images/medium_portrait/231/579166.jpg'
+      })
+    ];
+  }
 
   ngOnInit(): void {
   }
--- src/app/show-info/show-info.component.ts
+++ src/app/show-info/show-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { Show } from '../models/show.model';
 
 @Component({
@@ -7,16 +7,16 @@ import { Show } from '../models/show.model';
   styleUrls: ['./show-info.component.css']
 })
 export class ShowInfoComponent implements OnInit {
-  show: Show;
+  @Input() show: Show;
   constructor() {
-    this.show = new Show({name: 'Doctor Who',
-      language: 'English',
-      genres: ['Sci-Fi', ' Action', ' Adventure'],
-      id: 210,
-      summary: 'A show about something',
-      status: 'ongoing',
-      image: 'https://culvercitycrossroads.com/wp-content/uploads/2016/02/talent-show.jpg'
-    });
+    // this.show = new Show({name: 'Doctor Who',
+    //   language: 'English',
+    //   genres: ['Sci-Fi', ' Action', ' Adventure'],
+    //   id: 210,
+    //   summary: 'A show about something',
+    //   status: 'ongoing',
+    //   image: 'https://culvercitycrossroads.com/wp-content/uploads/2016/02/talent-show.jpg'
+    // });
   }
 
   ngOnInit(): void {
