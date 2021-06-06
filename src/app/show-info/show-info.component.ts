25436d508aea31bfaaf9190286b2eba7e5965c45
Heriz <herrypatel3369@gmail.com>
2021-06-06 14:04:20 -0400

    Quiz 14

--- src/app/show-info/show-info.component.ts
+++ src/app/show-info/show-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { Show } from '../models/show.model';
+import { PouchService } from '../services/pouch.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-show-info',
@@ -8,18 +10,14 @@ import { Show } from '../models/show.model';
 })
 export class ShowInfoComponent implements OnInit {
   @Input() show: Show;
-  constructor() {
-    // this.show = new Show({name: 'Doctor Who',
-    //   language: 'English',
-    //   genres: ['Sci-Fi', ' Action', ' Adventure'],
-    //   id: 210,
-    //   summary: 'A show about something',
-    //   status: 'ongoing',
-    //   image: 'https://culvercitycrossroads.com/wp-content/uploads/2016/02/talent-show.jpg'
-    // });
-  }
+  constructor(private pouch: PouchService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  addShow(): void {
+    this.pouch.addShow(this.show);
+  }
+  removeShow(): void {
+    this.pouch.removeShow(this.show);
+  }
 }
